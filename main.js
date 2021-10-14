da6a55cb27bb2f9b80b7666d320a77a3135aafad
Tarek Massini <tarek.jannis.03.tm@gmail.com>
2021-10-14 16:52:10 +0200

    cleanup of unnecessary awaits

--- main.js
+++ main.js
@@ -8,15 +8,14 @@ function randint(upperBound) {
 async function fetchAvailableComics() {
     const response = await fetch("https://xkcd.com")
     const text = await response.text()
-    const dom = await new JSDOM(text)
-    const url = await dom.window.document.querySelector("#middleContainer > a:nth-child(6)").textContent
+    const dom = new JSDOM(text)
+    const url = dom.window.document.querySelector("#middleContainer > a:nth-child(6)").textContent
     result = url.split("/")
     return parseInt(result[result.length-2])
 }
 
 async function generateXkcdLink() {
-    const availableComics = await fetchAvailableComics()
-    const url = await `https://xkcd.com/${randint(availableComics)}`
+    const url = `https://xkcd.com/${randint( await fetchAvailableComics())}`
     return url
 }
 
